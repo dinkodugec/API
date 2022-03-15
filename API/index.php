556a7d1857f3adddd7df7e7b829ac3770541bae1
Dinko <dinko.dugec@gmail.com>
2022-03-15 22:07:55 +0100

    init push

---
+++ API/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+$parts = explode("/", $path);
+
+$resource = $parts[2];
+
+$id = $parts[3] ?? null;
+
+echo $resource, ", ", $id;
+
+echo $_SERVER["REQUEST_METHOD"], "\n";
+
+if ($resource != "tasks") {
+    
+    //header("{$_SERVER['SERVER_PROTOCOL']} 404 Not Found");
+    http_response_code(404);
+    exit;
+}
